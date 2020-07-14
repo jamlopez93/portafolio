9d3dbf65dd6071bf1643ace5d1969c407d327af2
Mauricio <maurilo934@hotmail.com>
2020-07-14 12:47:38 -0500

    blog mitad de camino pre migracion (el dllo está en jobs)

--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+class blogpost(models.Model):
+    blog_title = models.CharField(max_length=50)
+    pub_date_blog = models.CharField(max_length=30)
+    blog_image = models.ImageField(upload_to='images/')
+    body = models.CharField(max_length=500)
+
+
+
+
