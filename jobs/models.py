99ca4beaf963e74dc669778f8cf6e63fb34873fc
Mauricio <maurilo934@hotmail.com>
2020-07-15 08:08:43 -0500

    migracion y cambios de job a blog

--- jobs/models.py
+++ jobs/models.py
@@ -6,11 +6,6 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
-class blogpost(models.Model):
-    blog_title = models.CharField(max_length=50)
-    pub_date_blog = models.CharField(max_length=30)
-    blog_image = models.ImageField(upload_to='images/')
-    body = models.CharField(max_length=500)
 
 
 
