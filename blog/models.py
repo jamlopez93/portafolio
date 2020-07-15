b38c6962459c8987ffef51218215aab5e5ecd2be
Mauricio <maurilo934@hotmail.com>
2020-07-15 08:31:49 -0500

    blog corregido y migrado

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class blogpost(models.Model):
-    blog_title = models.CharField(max_length=50)
-    pub_date_blog = models.CharField(max_length=30)
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=250)
+    pub_date_blog = models.DateTimeField()
     blog_image = models.ImageField(upload_to='images/')
-    body = models.CharField(max_length=500)
+    body = models.TextField()
 
