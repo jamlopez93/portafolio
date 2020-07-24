2ba68089b51cea706bb7e7dfb5b9eff0924b8e2a
Mauricio <maurilo934@hotmail.com>
2020-07-24 14:49:00 -0500

    Blog ddetalles

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,17 @@ from django.db import models
 
 class Blog(models.Model):
     blog_title = models.CharField(max_length=250)
+    def __str__(self):
+       return self.blog_title
     pub_date_blog = models.DateTimeField()
     blog_image = models.ImageField(upload_to='images/')
     body = models.TextField()
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date_blog.strftime('%b %e %Y')    
+   
+   
 
