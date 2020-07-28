1e935380838c961a2a0ec33e9571e5b784dd5aac
Mauricio <maurilo934@hotmail.com>
2020-07-28 12:49:36 -0500

    Static no funciona

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -123,8 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATICFILE_DIRS = [
+os.path.join(BASE_DIR, 'static'),
+'portafolio2/portafolio/static',
+
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
